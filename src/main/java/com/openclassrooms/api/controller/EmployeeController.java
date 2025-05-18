4019abf1f57c531e5e205d46983537c13042ac8c
Nana-Jacques <jeanjacquesnana00@gmail.com>
2025-05-19 00:30:06 +0100

    Modif EmployeeController

--- src/main/java/com/openclassrooms/api/controller/EmployeeController.java
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -16,12 +16,13 @@ import com.openclassrooms.api.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	/**
 	 * Create - Add a new employee
+	 * 
 	 * @param employee An object employee
 	 * @return The employee object saved
 	 */
@@ -29,59 +30,63 @@ public class EmployeeController {
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeService.saveEmployee(employee);
 	}
-	
-	
+
 	/**
-	 * Read - Get one employee 
+	 * Read - Get one employee
+	 * 
 	 * @param id The id of the employee
 	 * @return An Employee object full filled
 	 */
 	@GetMapping("/employee/{id}")
 	public Employee getEmployee(@PathVariable("id") final Long id) {
 		Optional<Employee> employee = employeeService.getEmployee(id);
-		if(employee.isPresent()) {
+		if (employee.isPresent()) {
 			return employee.get();
 		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Read - Get all employees
+	 * 
 	 * @return - An Iterable object of Employee full filled
 	 */
 	@GetMapping("/employees")
 	public Iterable<Employee> getEmployees() {
 		return employeeService.getEmployees();
 	}
-	
+
 	/**
 	 * Update - Update an existing employee
-	 * @param id - The id of the employee to update
+	 * 
+	 * @param id       - The id of the employee to update
 	 * @param employee - The employee object updated
 	 * @return
 	 */
 	@PutMapping("/employee/{id}")
 	public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
 		Optional<Employee> e = employeeService.getEmployee(id);
-		if(e.isPresent()) {
+		if (e.isPresent()) {
 			Employee currentEmployee = e.get();
-			
+
 			String firstName = employee.getFirstName();
-			if(firstName != null) {
+			if (firstName != null) {
 				currentEmployee.setFirstName(firstName);
 			}
 			String lastName = employee.getLastName();
-			if(lastName != null) {
-				currentEmployee.setLastName(lastName);;
+			if (lastName != null) {
+				currentEmployee.setLastName(lastName);
+				;
 			}
 			String mail = employee.getMail();
-			if(mail != null) {
+			if (mail != null) {
 				currentEmployee.setMail(mail);
 			}
 			String password = employee.getPassword();
-			if(password != null) {
-				currentEmployee.setPassword(password);;
+			if (password != null) {
+				currentEmployee.setPassword(password);
+				;
 			}
 			employeeService.saveEmployee(currentEmployee);
 			return currentEmployee;
@@ -89,16 +94,15 @@ public class EmployeeController {
 			return null;
 		}
 	}
-	
-	
+
 	/**
 	 * Delete - Delete an employee
+	 * 
 	 * @param id - The id of the employee to delete
 	 */
 	@DeleteMapping("/employee/{id}")
 	public void deleteEmployee(@PathVariable("id") final Long id) {
 		employeeService.deleteEmployee(id);
 	}
-	
 
 }
