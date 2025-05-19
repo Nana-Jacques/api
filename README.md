6c1d3c24e1102f1ac0f313e251bab935617187ae
Nana-Jacques <jeanjacquesnana00@gmail.com>
2025-05-19 01:11:29 +0100

    Commentaire dans Employee.javaù

---
+++ README.md
@@ -0,0 +1,3 @@
+Ce projet impélement une api de gestion des employés avec H2
+
+Les tests ont été fait avec PostMan
--- src/main/java/com/openclassrooms/api/model/Employee.java
+++ src/main/java/com/openclassrooms/api/model/Employee.java
@@ -8,23 +8,23 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-@Data //inutile d'ajouter les getters et les setters
-@Entity //indique que la classe correspond à une tavle de la BD
-@Table(name = "employees") //indique le nom de la table assoicé
+@Data // inutile d'ajouter les getters et les setters
+@Entity // indique que la classe correspond à une table de la BD
+@Table(name = "employees") // indique le nom de la table assoicé
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column (name = "first_name")
+    @Column(name = "first_name")
     private String firstName;
 
-    @Column (name ="last_name") // Pour indique le nom du champ correspondant en bd
+    @Column(name = "last_name") // Pour indique le nom du champ correspondant en bd
     private String lastName;
 
     private String mail;
 
-    private String password; 
+    private String password;
 
 }
